30bdc13677263ab758a0bfc6738199ae69af4ff9
Tofiq Quadri <tofiqquadri@ymail.com>
2020-08-27 12:02:31 +0530

    # Changelog and package.json updated
    # NgxCsvParser - v0.0.5 (27/07/2020)
    * New Features:
    * Compliance updated as per RFC 4180
    * RFC 4180 https://tools.ietf.org/html/rfc4180
    
    # NgxCsvParser - v0.0.4 (27/07/2020)
    * New Features:
    * Compliance added as per RFC 4180
    * RFC 4180 https://tools.ietf.org/html/rfc4180
    * Not CSV Files can have values containing (,) and other characters as per RFC 4180

--- projects/ngx-csv-parser/src/lib/ngx-csv-parser.service.ts
+++ projects/ngx-csv-parser/src/lib/ngx-csv-parser.service.ts
@@ -3,135 +3,153 @@ import { Observable, Observer } from 'rxjs';
 import { NgxCSVParserError } from './_model/ngx-csv-parser-error.interface';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class NgxCsvParser {
 
-  constructor() { 
-    console.log(`
+    constructor() {
+        console.log(`
       *****************************************************
       ***      For Custom Software/Web development      ***
       *** Reach out to us at contact@developershive.com ***
+      ***  Support us https://patreon.com/tofiqquadri/  ***
       ***     Visit us at https://developershive.com    ***
       *****************************************************
-    `);    
-  }
+    `);
+    }
 
-  private defaultCSVParserConfig = {
-    header: true,
-    delimiter: ','
-  };
-
-  parse(csvFile: File, config: CSVParserConfig): Observable<Array<any>> {
-
-    config = {
-      ...this.defaultCSVParserConfig,
-      ...config
+    private defaultCSVParserConfig = {
+        header: true,
+        delimiter: ','
     };
 
-    const ngxCSVParserObserver = Observable.create((observer: Observer<Array<any> | NgxCSVParserError>) => {
-      try {
+    parse(csvFile: File, config: CSVParserConfig): Observable<Array<any> | NgxCSVParserError> {
 
-        let csvRecords = null;
+        config = {
+            ...this.defaultCSVParserConfig,
+            ...config
+        };
 
-        if (this.isCSVFile(csvFile)) {
+        const ngxCSVParserObserver = new Observable((observer: Observer<Array<any> | NgxCSVParserError>) => {
+            try {
+                let csvRecords = null;
 
-          const reader = new FileReader();
-          reader.readAsText(csvFile);
+                if (this.isCSVFile(csvFile)) {
 
-          reader.onload = () => {
-            const csvData = reader.result;
-            const csvRecordsArray = (csvData as string).trim().split(/\r\n|\n/);
+                    const reader = new FileReader();
+                    reader.readAsText(csvFile);
 
-            const headersRow = this.getHeaderArray(csvRecordsArray, config);
+                    reader.onload = () => {
+                        const csvData = reader.result;
+                        const csvRecordsArray = this.csvStringToArray((csvData as string).trim(), config.delimiter);
 
-            csvRecords = this.getDataRecordsArrayFromCSVFile(csvRecordsArray, headersRow.length, config);
+                        const headersRow = this.getHeaderArray(csvRecordsArray);
 
-            observer.next(csvRecords);
-            observer.complete();
-          };
+                        csvRecords = this.getDataRecordsArrayFromCSVFile(csvRecordsArray, headersRow.length, config);
 
-          reader.onerror = () => {
-            this.badCSVDataFormatErrorHandler(observer);
-          };
+                        observer.next(csvRecords);
+                        observer.complete();
+                    };
 
-        } else {
-          this.notCSVFileErrorHandler(observer);
-        }
+                    reader.onerror = () => {
+                        this.badCSVDataFormatErrorHandler(observer);
+                    };
 
-      } catch (error) {
-        this.unknownCSVParserErrorHandler(observer);
-      }
-    });
+                } else {
+                    this.notCSVFileErrorHandler(observer);
+                }
 
-    return ngxCSVParserObserver;
-  }
+            } catch (error) {
+                this.unknownCSVParserErrorHandler(observer);
+            }
+        });
 
-  getDataRecordsArrayFromCSVFile(csvRecordsArray: any, headerLength: any, config: any) {
-    const dataArr = [];
-    const headersArray = (csvRecordsArray[0] as string).split(config.delimiter);
-
-    const startingRowToParseData = config.header ? 1 : 0;
-
-    for (let i = startingRowToParseData; i < csvRecordsArray.length; i++) {
-      const data = (csvRecordsArray[i] as string).split(config.delimiter);
-
-      if (data.length === headerLength && config.header) {
-
-        const csvRecord = {};
-
-        for (let j = 0; j < data.length; j++) {
-          csvRecord[headersArray[j]] = data[j].trim();
-        }
-        dataArr.push(csvRecord);
-      } else {
-        dataArr.push(data);
-      }
+        return ngxCSVParserObserver;
     }
-    return dataArr;
-  }
 
-  isCSVFile(file: any) {
-    return file.name.endsWith('.csv');
-  }
-
-  getHeaderArray(csvRecordsArr: any, config: any) {
-    const headers = (csvRecordsArr[0] as string).split(config.delimiter);
-    const headerArray = [];
-    for (const header of headers) {
-      headerArray.push(header);
+    csvStringToArray(csvDataString: string, delimiter: string) {
+        const regexPattern = new RegExp((`(\\${delimiter}|\\r?\\n|\\r|^)(?:\"((?:\\\\.|\"\"|[^\\\\\"])*)\"|([^\\${delimiter}\"\\r\\n]*))`),"gi")
+        let matchedPatternArray = regexPattern.exec(csvDataString);
+        const resultCSV = [[]];
+        while (matchedPatternArray) {
+            if (matchedPatternArray[1].length && matchedPatternArray[1] !== delimiter) {
+                resultCSV.push([]);
+            }
+            const cleanValue = matchedPatternArray[2] ?
+                matchedPatternArray[2].replace(new RegExp( "[\\\\\"](.)", "g" ), '$1') : matchedPatternArray[3];
+            resultCSV[resultCSV.length - 1].push(cleanValue);
+            matchedPatternArray = regexPattern.exec(csvDataString);
+        }
+        return resultCSV;
     }
-    return headerArray;
-  }
 
-  notCSVFileErrorHandler(observer: Observer<any>) {
-    const ngcCSVParserError: NgxCSVParserError = new NgxCSVParserError();
-    ngcCSVParserError.type = 'NOT_A_CSV_FILE';
-    ngcCSVParserError.message = 'Selected file is not a csv File Type';
-    ngcCSVParserError.code = 2;
-    observer.error(ngcCSVParserError);
-  }
+    getDataRecordsArrayFromCSVFile(csvRecordsArray: any, headerLength: any, config: any) {
+        const dataArr = [];
+        const headersArray = csvRecordsArray[0];
 
-  unknownCSVParserErrorHandler(observer: Observer<any>) {
-    const ngcCSVParserError: NgxCSVParserError = new NgxCSVParserError();
-    ngcCSVParserError.type = 'UNKNOWN_ERROR';
-    ngcCSVParserError.message = 'Unknown error. Please refer to official documentation for library usage.';
-    ngcCSVParserError.code = 404;
-    observer.error(ngcCSVParserError);
-  }
+        const startingRowToParseData = config.header ? 1 : 0;
 
-  badCSVDataFormatErrorHandler(observer: Observer<any>) {
-    const ngcCSVParserError: NgxCSVParserError = new NgxCSVParserError();
-    ngcCSVParserError.type = 'BAD_CSV_DATA_FORMAT';
-    ngcCSVParserError.message = 'Unable to parse CSV File';
-    ngcCSVParserError.code = 1;
-    observer.error(ngcCSVParserError);
-  }
+        for (let i = startingRowToParseData; i < csvRecordsArray.length; i++) {
+            const data = csvRecordsArray[i];
+
+            if (data.length === headerLength && config.header) {
+
+                const csvRecord = {};
+
+                for (let j = 0; j < data.length; j++) {
+                    csvRecord[headersArray[j]] = data[j].trim();
+                }
+                dataArr.push(csvRecord);
+            } else {
+                dataArr.push(data);
+            }
+        }
+        return dataArr;
+    }
+
+    isCSVFile(file: any) {
+        return file.name.endsWith('.csv');
+    }
+
+    getHeaderArray(csvRecordsArr: any) {
+        const headers = csvRecordsArr[0];
+        const headerArray = [];
+        for (const header of headers) {
+            headerArray.push(header);
+        }
+        return headerArray;
+    }
+
+    notCSVFileErrorHandler(observer: Observer<any>) {
+        const ngcCSVParserError: NgxCSVParserError =
+            this.errorBuilder('NOT_A_CSV_FILE', 'Selected file is not a csv File Type.', 2);
+        observer.error(ngcCSVParserError);
+    }
+
+    unknownCSVParserErrorHandler(observer: Observer<any>) {
+        const ngcCSVParserError: NgxCSVParserError =
+            this.errorBuilder('UNKNOWN_ERROR', 'Unknown error. Please refer to official documentation for library usage.', 404);
+        observer.error(ngcCSVParserError);
+    }
+
+    badCSVDataFormatErrorHandler(observer: Observer<any>) {
+        const ngcCSVParserError: NgxCSVParserError =
+            this.errorBuilder('BAD_CSV_DATA_FORMAT', 'Unable to parse CSV File.', 1);
+        observer.error(ngcCSVParserError);
+    }
+
+    errorBuilder(type: string, message: any, code: any): NgxCSVParserError {
+        const ngcCSVParserError: NgxCSVParserError = new NgxCSVParserError();
+        ngcCSVParserError.type = type;
+        ngcCSVParserError.message = message;
+        ngcCSVParserError.code = code;
+        return ngcCSVParserError;
+    }
 }
 
 class CSVParserConfig {
-  header?: boolean;
-  delimiter?: string;
+    header?: boolean;
+    delimiter?: string;
 
-  constructor() { }
+    constructor() { }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { NgxCSVParserError } from 'projects/ngx-csv-parser/src/public-api';
 export class AppComponent {
 
   csvRecords: any[] = [];
-  header = false;
+  header: boolean = false;
 
   constructor(private ngxCsvParser: NgxCsvParser) {
   }
@@ -22,6 +22,7 @@ export class AppComponent {
   fileChangeListener($event: any): void {
 
     const files = $event.srcElement.files;
+    this.header = (this.header as unknown as string) === 'true' || this.header === true;
 
     this.ngxCsvParser.parse(files[0], { header: this.header, delimiter: ',' })
       .pipe().subscribe((result: Array<any>) => {
